ec2b122d21f6b8a0bae748e2d7f2d3c6df0a4e72
Steven Horne <123stevenhorne@gmail.com>
2025-09-02 16:06:07 +0100

    second modification to SearchBar

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -10,6 +10,7 @@ export default function SearchBar({ search, setSearch }: SearchBarProps) {
     return (
         <div>
             <input
+            className='search-bar'
                 value={search}
                 placeholder="Search for team..."
                 onChange={e => setSearch(e.target.value)}
