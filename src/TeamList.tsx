4df40d2513859430c2cecbd1f073226457812161
Steven Horne <stevenhorne@Stevens-MacBook-Air.local>
2026-07-23 20:48:34 +0100

    Added a empty search display

--- src/TeamList.tsx
+++ src/TeamList.tsx
@@ -12,24 +12,32 @@ export default function TeamList({ teamNames, search, getRoster }: TeamListProps
   return (
 
     <ul className='team-list'>
-      {teamNames
-        .filter((team) =>
+      {/* 1. Filter the list first */}
+      {teamNames.filter((team) =>
+        team.name.toLowerCase().includes(search.toLowerCase())
+      ).length === 0 ? (
+        /* 2. Show this if empty */
+        <li>No teams found</li>
+      ) : (
+        /* 3. Otherwise, filter again and map */
+        teamNames
+          .filter((team) =>
+            team.name.toLowerCase().includes(search.toLowerCase())
+          )
+          .map((team) => (
+            <li key={team.name}>
+              <img
+                src={team.logo}
+                alt={team.name}
+                style={{ width: 25, height: 25 }}
+              />
+              <button className="team-button" onClick={() => getRoster(team.abbre)}>
+                {team.name} {team.abbre}</button>
+              <div ></div>
 
-          team.name.toLowerCase().includes(search.toLowerCase())
-        )
-        .map((team) => (
-          <li key={team.name}>
-            <img
-              src={team.logo}
-              alt={team.name}
-              style={{ width: 25, height: 25 }}
-            />
-            <button className="team-button" onClick={() => getRoster(team.abbre)}>
-              {team.name} {team.abbre}</button>
-            <div ></div>
-
-          </li>
-        ))}
+            </li>
+          ))
+      )}
     </ul>
   )
 
